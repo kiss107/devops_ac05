189136d5ebb7b9ca10a2ad713ff70e1a019aa915
Ingrid Contreras <68926649+kiss107@users.noreply.github.com>
2020-10-29 19:50:47 -0300

    configurando

--- setup.py
+++ setup.py
@@ -8,5 +8,15 @@ c.execute ('' 'CREATE TABLE ações
 
 c.execute ("INSERT INTO stocks VALUES('2006-01-05', 'BUY', 'RHAT', 100,35.14) ")
 
+conn.commit ()import sqlite3
+
+conn = sqlite3.connect ('exemplo.db')
+c = conn.cursor ()
+
+c.execute ('''CREATE TABLE ações
+(texto de data, texto trans, texto de símbolo, quantidade real, preço real) ''')
+
+c.execute ("INSERT INTO stocks VALUES('2006-01-05', 'BUY', 'RHAT', 100,35.14) ")
+
 conn.commit ()
 conn.close ()
